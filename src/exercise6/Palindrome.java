b5efcd0c20de561c11ab2cdbf195d590f380ca03
pesho217 <>
2021-11-18 22:17:53 +0000

    Files updated

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -5,5 +5,33 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        int counter = 0;
+        int number = input.nextInt();
+        int number1 = number;
+        int reverse = 0;
+        int normal = number;
+        
+        do {
+          number = number/10;
+          counter++;
+
+        } while (number != 0);
+        int array[] = new int[counter];
+
+        for (int i = 0; i < counter; i++) {
+          array[i] = number1%10;
+          number1 = number1/10;
+        }
+        for (int i = 0;i < counter; i++) {
+          reverse = reverse*10;
+          reverse = array[i] + reverse;
+         
+        }
+        if (normal == reverse) {
+          System.out.println("Palindrome: true");
+        } else {
+          System.out.println("Palindrome: false");
+        }
     }
 }
--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,27 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-    }
+        int kur = 0;
+        int count = 0;
+        do {
+        String password = input.nextLine();
+        if (password.length() >= 8 ) {
+        
+        for(int i = 0; i < password.length(); i++){
+        if (Character.isLetterOrDigit(password.charAt(i))){
+            for(int j = 0; j < password.length(); j++) {
+        if(Character.isDigit(password.charAt(i))) {
+          count++;
+          if(count > 1){
+             kur = 1;
+            }
+           }
+          } 
+         }
+        }
+      }
+      
+    }while(kur != 1);
+        System.out.println("Password valid!");
+  }
 }
